9ca401cc0cb40bd3d1a7c87f0f73101e8d3b5dc9
Aditya Eka B <aditya.eka409@gmail.com>
2023-11-25 19:34:01 +0700

    update

--- consumer.py
+++ consumer.py
@@ -2,12 +2,38 @@ from kafka import KafkaConsumer
 import numpy as np
 import cv2
 from ultralytics import YOLO
+import json
+import time
+import base64
 
 consumer = KafkaConsumer('video-stream', bootstrap_servers='localhost:9092')
+average_latency = 0
+frame_count = 0
+total_latency = 0
+
 def get_video():
   for message in consumer:
-    frame = np.frombuffer(message.value, dtype='uint8')
+    data = json.loads(message.value)
 
+    global total_latency
+    global frame_count
+    global average_latency
+    
+    latency = (time.time() - data['timestamp']) * 1000
+    total_latency += latency
+    frame_count += 1
+    average_latency = total_latency / frame_count
+
+    print()
+    print('-----------------------')
+    print(f'Latency: {latency} ms')
+    print(f'Total latency: {total_latency} ms')
+    print(f'Frame count: {frame_count}')
+    print(f'Average latency: {average_latency} ms')
+
+    image_bytes = base64.b64decode(data['image'])
+
+    frame = np.frombuffer(image_bytes, dtype='uint8')
     image = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
     model = YOLO('yolov8n.pt')
@@ -15,10 +41,14 @@ def get_video():
     results = model.track(image, persist=True)
     annotated_frame = results[0].plot()
 
+    print('-----------------------')
+
     yield annotated_frame
+
 for value in get_video():
   cv2.imshow('frame', value)
      
   if cv2.waitKey(1) == ord('q'):
     break
+
 cv2.destroyAllWindows()
--- producer.py
+++ producer.py
@@ -1,6 +1,8 @@
 import time
 import sys
 import cv2
+import json
+import base64
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
@@ -8,28 +10,49 @@ from kafka.errors import KafkaError
 producer = KafkaProducer(bootstrap_servers=('localhost:9092'))
 topic = 'video-stream'
 
+def frame_to_json(frame, timestamp):
+    # Encode frame to JPEG
+    ret, jpeg = cv2.imencode('.jpeg', frame)
+
+    # Convert JPEG bytes to base64 string
+    image_str = base64.b64encode(jpeg.tobytes()).decode('utf-8')
+
+    # Create data dictionary
+    data = {
+      'image': image_str,
+      'timestamp': timestamp,
+    }
+
+    # Convert data dictionary to JSON string
+    json_str = json.dumps(data).encode('utf-8')
+
+    return json_str
+
 def emit_video(path_to_video):
-    print('start')
+  print('start')
+  video = cv2.VideoCapture(path_to_video)
 
-    video = cv2.VideoCapture(path_to_video)
+  while video.isOpened():
+    timestamp = time.time()
 
-    while video.isOpened():
-        success, frame = video.read()
-        if not success:
-            break
+    success, frame = video.read()
+    if not success:
+      break
 
-        # png might be too large to emit
-        data = cv2.imencode('.jpeg', frame)[1].tobytes()
+    timestamp = time.time()  # Replace with your method of getting the timestamp
 
-        future = producer.send(topic, data)
-        try:
-            future.get(timeout=10)
-        except KafkaError as e:
-            print(e)
-            break
+    # Convert frame to JSON
+    json_str = frame_to_json(frame, timestamp)
 
-        print('.', end='', flush=True)
+    future = producer.send(topic, json_str)
+    try:
+      future.get(timeout=10)
+    except KafkaError as e:
+      print(e)
+      break
 
-emit_video('data/mot1609.webm')
-# emit_video('data/test.mp4')
+    print('.', end='', flush=True)
+
+# emit_video(0)
+emit_video('data/test.mp4')
 # emit_video(0)
